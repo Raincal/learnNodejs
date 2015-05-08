529a2f729ebcbc1898cd1d2f286f97f6b8ad5efc
Raincal <cyj94228@gamil.com>
2015-05-09 01:14:40 +0800

    process_cwd

---
+++ process_cwd.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Yujia on 2015/5/9.
+ */
+console.log(process.cwd());
---
+++ subdir/cwd_subdir.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Yujia on 2015/5/9.
+ */
+console.log('dirname:'+__dirname); //dirname -->js所在目录
+console.log('cwd:'+process.cwd()); //cwd() -->执行node命令时所在目录
+
+//cwd ->> current working dir
